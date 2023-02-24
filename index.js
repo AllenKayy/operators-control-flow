abd0ff7293bf64dd7b38c199d291bed9642567ba
AllenKayy <allisonoyinkansola1@gmail.com>
2023-02-24 15:02:09 +0100

    modified

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Question No 3
+
 let scienceSubjects, socialScienceSubjects, artsSubjects, generalSubjects, message, invalidMessage, classGroup;
 
 message = "Welcome! You're"
@@ -8,8 +10,6 @@ socialScienceSubjects = "Accounting, Commerce, Marketing, Geography"
 artsSubjects = "Government, Economics, Literature, History"
 generalSubjects = "English, Mathematics"
 
-
-classGroup = artsSubjects;
 
 const classGroupCategory = () => {
     if (classGroup === scienceSubjects) {
@@ -23,4 +23,20 @@ const classGroupCategory = () => {
     }
 }
 
+classGroup = artsSubjects;
 classGroupCategory();
+
+// Question No 5
+
+let num = 40
+
+nearestPowerOf2 = (num) => {
+    let pwr = 1;
+    for (let i = 0; pwr <= num; i++) {
+      pwr = 2 ** i;
+    }
+    return (pwr / 2);
+  }
+
+ pwr = nearestPowerOf2(num)
+ console.log(`The number ${pwr} is the power of 2 nearest to ${num}.`);
